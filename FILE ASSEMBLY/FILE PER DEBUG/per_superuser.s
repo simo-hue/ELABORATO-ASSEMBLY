949c0e4cef9f65b53aa0e292713235bf062ad719
davoddino <david.cavada.lab@gmail.com>
2023-05-18 22:21:58 +0200

    modifiche

--- FILE ASSEMBLY/FILE PER DEBUG/per_superuser.s
+++ FILE ASSEMBLY/FILE PER DEBUG/per_superuser.s
@@ -1,38 +1,39 @@
-section .data
-    message_normal db 'ciao sei un utente normale', 0
-    message_superuser db 'ciao superuser', 0
-    message_ptr db 0
+.section .data
+    message_normal: .string "ciao sei un utente normale"
+    message_superuser: .string "ciao superuser"
+    message_ptr: .long 0
 
-section .text
-    global _start
+.section .text
+    .global _start
 
 _start:
-    ; Controllo se sono stati passati argomenti da riga di comando
-    cmp dword [esp+4], 1       ; Controllo se il conteggio degli argomenti è uguale a 1
-    jne normal_user            ; Se il conteggio non è uguale a 1, vai alla sezione per l'utente normale
+    # Controllo se sono stati passati argomenti da riga di comando
+    movl 4(%esp), %eax       # Carica il conteggio degli argomenti nella register eax
+    cmpl $1, %eax            # Controllo se il conteggio degli argomenti è uguale a 1
+    jne normal_user          # Se il conteggio non è uguale a 1, vai alla sezione per l'utente normale
     
-    ; Se è stato passato un argomento, controlliamo il suo valore
-    cmp dword [esp+8], 2244    ; Controllo se il primo argomento è uguale a 2244
-    jne normal_user            ; Se il primo argomento non è uguale a 2244, vai alla sezione per l'utente normale
+    # Se è stato passato un argomento, controlliamo il suo valore
+    cmpl $2244, 8(%esp)      # Controllo se il primo argomento è uguale a 2244
+    jne normal_user          # Se il primo argomento non è uguale a 2244, vai alla sezione per l'utente normale
     
-    ; Se il controllo precedente fallisce, stampa "ciao superuser"
-    mov eax, message_superuser
+    # Se il controllo precedente fallisce, stampa "ciao superuser"
+    movl $message_superuser, %eax
     jmp print_message
 
 normal_user:
-    ; Se il controllo precedente fallisce o non ci sono argomenti, stampa "ciao sei un utente normale"
-    mov eax, message_normal
+    # Se il controllo precedente fallisce o non ci sono argomenti, stampa "ciao sei un utente normale"
+    movl $message_normal, %eax
 
 print_message:
-    ; Stampa il messaggio
-    mov ebx, 1                  ; File descriptor (1 per stdout)
-    mov ecx, eax                ; Puntatore al messaggio
-    mov edx, 22                 ; Lunghezza del messaggio
-    mov eax, 4                  ; Chiamata di sistema per scrivere
-    int 0x80                    ; Interruzione del kernel per eseguire la chiamata di sistema
+    # Stampa il messaggio
+    movl $1, %ebx           # File descriptor (1 per stdout)
+    movl %eax, %ecx         # Puntatore al messaggio
+    movl $22, %edx          # Lunghezza del messaggio
+    movl $4, %eax           # Chiamata di sistema per scrivere
+    int $0x80               # Interruzione del kernel per eseguire la chiamata di sistema
 
 exit:
-    ; Termina il programma
-    mov eax, 1                  ; Chiamata di sistema per terminare il programma
-    xor ebx, ebx                ; Codice di uscita (0)
-    int 0x80                    ; Interruzione del kernel per eseguire la chiamata di sistema
+    # Termina il programma
+    movl $1, %eax           # Chiamata di sistema per terminare il programma
+    xorl %ebx, %ebx         # Codice di uscita (0)
+    int $0x80               # Interruzione del kernel per eseguire la chiamata di sistema
